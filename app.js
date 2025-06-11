b5ababa8f3e0689ff21ccebf17394f0df8bc29fa
Sameer <sameermalik63901@gmail.com>
2025-06-11 15:07:41 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 // CORS Middleware
 app.use(
   cors({
-    origin:'http://localhost:3000',
+    origin:'https://funny-baklava-10d1b5.netlify.app',
     credentials: true,
   })
 );
